be4e5f96f781b4f0240ecfb4dcce15a37edac679
TheNetherKingdom <thenetherkingdom@gmail.com>
2022-10-02 05:57:00 +0300

    Added Previously skipped features
    Didnt upload full version last time, so adding it now.

--- user_selection.py
+++ user_selection.py
@@ -1,21 +1,46 @@
+
+
+def get_input(msg: str = "") -> int:
+  while not (k := input(msg).strip()).isnumeric():
+    print("Invalid Input, Enter a number.")
+  return int(k)
+  
+def get_marks(subjects: list) -> dict:
+  question = 'Enter your {} Marks: '
+  ask_with = lambda x: get_input(question.format(x))
+  
+  return {name: ask_with(name) for name in subjects}
+  
+def pad_right(a: list) -> list:
+  k = max(map(len, a))
+  return [i.ljust(k) for i in a]
+
+"""
+def get_marks_alternative(subjects: list) -> list:
+  question = 'Enter your {} Marks: '
+  
+  answers = []
+  for i in subjects:
+    ans
+"""
 from typing import Union, List
 
 class HandleOptions:
-  def __init__(self, options: list, title: str = "", start: int = 1, 
-                *, prefix: str = "\t", sep: str = ""):
+  def __init__(self, options: list, start: int = 0, 
+                *, prefix: str = "\t", sep: str = ". "):
     self.options: dict = dict(enumerate(options, start=start))
-    self.title = title or 'Choose one option -'
+    
     self.prefix = prefix
     self.sep = sep
   
-  
-  @staticmethod 
+  """
+  @staticmethod
   def show(*args, *, prefix: str = "", **kwargs) -> None:
     print(prefix, *args, **kwargs)
-  
+  """
   
   def __repr__(self) -> str:
-    string = (self.prefix + self.sep.join(item) for item in self.options. items())
+    string = (self.prefix + self.sep.join(map(str, item)) for item in self.options. items())
     return "\n".join(string)
   
   @staticmethod
@@ -25,23 +50,28 @@ class HandleOptions:
     
     print(msg)
   
-  def pick(self, msg: str = "", default = None):
-    print(self.title)
-    print(self)
+  def pick(self, msg: str = "", default = None, *, show: bool = True):
+    if show:
+       print('Choose one option -')
+       print(self)
     
-    clean_input = lambda x: get_input(x).strip().lower()
-    
-    return self.options.get(clean_input(msg), default)
+    return self.options.get(get_input(msg), default)
   
   def assert_pick(self, /, *args, **kwargs):
-    while (k := self.pick(*args, **kwargs)) is None:
-      print("Invalid Input. Enter a valid option")
+    k = self.pick()
+    while k is None:
+      print("Invalid Option. Try again.")
+      k = self.pick(show=False)
     
     return k
 
-if __name__ == "__main__":
-   options = ['Exam', 'Monthly Test']
-   marks = ["Maths", "Sst", "English", "Hindi", "Science"]
+def main():
+  options = ['Exam', 'Monthly Test']
+  marks = ["Maths", "Sst", "English", "Hindi", "Science"]
 
-   subjects = HandleOptions(options)
-   user = subjects.assert_pick("Enter Answer: ")
+  subjects = HandleOptions(options, 1)
+  user = subjects.assert_pick("Enter Answer: ")
+  print(f"\nSelected \'{user}\'.")
+
+  print()
+  e = get_marks(pad_right(marks))
